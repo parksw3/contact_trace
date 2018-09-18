c40c1bc2e0e75d1b5ef8a2e2a9b0a1cbcb0121f0
unknown <parksw3@mcmaster.ca>
2018-09-18 00:46:40 -0400

    update

--- analysis/temporal_effect.R
+++ analysis/temporal_effect.R
@@ -1,12 +1,9 @@
-source("ebola_param_seminr.R")
 library(deSolve)
 library(dplyr)
 library(ggplot2); theme_set(theme_bw())
 library(gridExtra)
 
-intrinsic_fun <- function(tau) {
-    sigma*gamma/(gamma-sigma) * (exp(-sigma*tau)-exp(-gamma*tau))
-}
+source("ebola_param_seminr.R")
 
 ## ode
 
@@ -20,85 +17,110 @@ seir.ode <- function(t, y, parms) {
     })
 }
 
+## we want to simulate equilibrium state!
+seirs.ode <- function(t, y, parms) {
+	with(as.list(c(y, parms)), {
+		dS <- gamma * I -beta/N*S*I
+		dE1 <- beta/N*S*I - m.sigma*sigma*E1
+		dE2 <- m.sigma*sigma*E1 - m.sigma*sigma*E2
+		dI <- m.sigma*sigma*E2 - gamma*I
+		list(c(dS, dE1, dE2, dI))
+	})
+}
+
+sumfun <- function(out,
+				   tmax,
+				   t,
+				   tauby=5) {
+	incidence <- beta*out$S*out$I/N
+	c.incidence <- cumsum(incidence)
+	
+	out$incidence <- incidence
+	
+	genden <- hist(gen, breaks=t, plot=FALSE)$density
+	
+	censor <- function(tau, tmax) {
+		m <- which(t==tmax)
+		total <- c.incidence[m]
+		
+		i <- which(t==tau)
+		
+		beta*sum(incidence[1:(m-i+1)] * out$S[i:m]/N)/total*genden[i]
+	}
+	
+	mean.censor <- function(tmax) {
+		if (tmax<=0){
+			return(0)
+		} else{
+			t <- t[t <= tmax]
+			
+			dc <- sapply(t, censor, tmax=tmax)
+			return(sum(dc*t)/sum(dc))
+		}
+	}
+	
+	tau <- seq(0, tmax, tauby)
+	cm <- sapply(tau, mean.censor)
+	
+	censor.df <- data.frame(
+		t=tau
+		, mean=cm
+	)
+}
+
 parms <- c(beta=beta, sigma=sigma, m.sigma=m.sigma, gamma=gamma, N=N)
-y <- c(S = N-1, E1=0, E2=0, I=10)
+y <- c(S = N-10, E1=0, E2=0, I=10)
 
-dt <- 1
-tmax <- 300
+dt <- 0.1
+tmax <- 305
 t <- seq(0, tmax, dt)
-out <- as.data.frame(ode(y, t, seir.ode, parms))
-names(out)[1] <- "t"
+out1 <- as.data.frame(ode(y, t, seir.ode, parms))
+out2 <- as.data.frame(ode(y, t, seirs.ode, parms))
+out3 <- as.data.frame(ode(unlist(tail(out2, 1))[-1] , t, seirs.ode, parms))
+out3 <- as.data.frame(ode(unlist(tail(out3, 1))[-1] , t, seirs.ode, parms))
+names(out1)[1] <- names(out2)[1] <- names(out3)[1] <- "t"
 
-incidence <- beta*out$S*out$I/N
-c.incidence <- cumsum(incidence)
+outlist <- list(out1, out3)
 
-out$incidence <- incidence
+gensum <- lapply(outlist, sumfun, tmax=305, t=t, tauby=5)
 
-genden <- hist(gen, breaks=t, plot=FALSE)$density
+# gensum_long <- sumfun(out4, tmax=10000, t=0:10000, tauby=1000)
 
-censor <- function(tau, tmax) {
-    m <- which(t==tmax)
-    total <- c.incidence[m]
-    
-    i <- which(t==tau)
-    
-    beta*sum(incidence[1:(m-i+1)] * out$S[i:m]/N)/total*genden[i]
-}
+outdata <- bind_rows(out1, out3, .id="type") %>%
+	mutate(
+		incidence=beta*S*I/N,
+		type=factor(type, label=c("epidemic", "equilibrium"))
+	)
 
-mean.censor <- function(tmax) {
-    if (tmax<=0){
-        return(0)
-    } else{
-        t <- t[t <= tmax]
-        
-        dc <- sapply(t, censor, tmax=tmax)
-        return(sum(dc*t)/sum(dc))
-    }
-}
-
-tau <- seq(0, tmax, 5)
-cm <- sapply(tau, mean.censor)
-
-censor.df <- data.frame(
-    t=tau
-    , mean=cm
-)
-
-exp.df <- data.frame(
-    t=t[which(t==100)],
-    incidence=incidence[which(t==100)],
-    mean=cm[which(tau==100)]
-)
-
-out.shade <- rbind(out[out$t <= 100, c("t", "incidence")], c(100, 0), c(0,0))
-colnames(out.shade)[1] <- "t"
-
-g1 <- ggplot(out, aes(t, incidence)) +
+g1 <- ggplot(outdata, aes(t, incidence)) +
     geom_line(lwd=1.2) +
-    geom_polygon(data=out.shade, alpha=0.2) +
-    geom_segment(data=data.frame(t=100, incidence=0, tend=100, iend=out$incidence[which(t==100)]), aes(xend=tend, yend=iend),
-                 lty=1) +
-    geom_point(data=exp.df, size=4) +
-    scale_x_continuous(expand=c(0,0)) +
-    scale_y_continuous("daily incidence", expand=c(0,0), limits=c(0, 650)) +
+    scale_x_continuous(expand=c(0,0), limits=c(0, 300), breaks=seq(0, 250, by=50)) +
+    scale_y_continuous("daily incidence", expand=c(0,0), limits=c(-5, 2000)) +
+	facet_wrap(~type) +
     theme(
         panel.grid=element_blank(),
         axis.title.x=element_blank(),
-        plot.margin=unit(c(5.5, 10, 5.5, 5.5), "points")
+        plot.margin=unit(c(5.5, 10, 5.5, 5.5), "points"),
+        strip.background = element_blank(),
+        panel.spacing = unit(0, "cm")
     )
 
-g2 <- ggplot(censor.df, aes(t, mean)) +
+gendata <- gensum %>%
+	bind_rows(.id="type")
+
+g2 <- ggplot(gendata, aes(t, mean)) +
     geom_line(lwd=1.2) +
-    geom_point(data=exp.df, size=4) +
-    geom_segment(data=data.frame(t=100, mean=0, tend=100, iend=censor.df$mean[which(censor.df$t==100)]), aes(xend=tend, yend=iend),
-                 lty=1) +
     geom_hline(yintercept = 1/sigma + 1/gamma, lty=2) +
-    scale_x_continuous("time (days)", expand=c(0,0)) +
-    scale_y_continuous("mean observed GI", expand=c(0,0), limits=c(0, 1/sigma+1/gamma+1)) +
-    theme(
+    scale_x_continuous("time (days)", expand=c(0,0), limits=c(0, 300), breaks=seq(0, 250, by=50)) +
+    scale_y_continuous("mean observed GI", expand=c(0,0), limits=c(0, 1/sigma+1/gamma+4)) +
+	facet_wrap(~type) +
+	theme(
         panel.grid=element_blank(),
-        axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0)),
-        plot.margin=unit(c(5.5, 10, 5.5, 5.5), "points")
+        axis.title.y = element_text(margin = margin(t = 0, r = 13.2, b = 0, l = 0)),
+        plot.margin=unit(c(5.5, 10, 5.5, 5.5), "points"),
+        strip.background = element_blank(),
+        strip.text = element_blank(),
+        panel.spacing = unit(0, "cm")
     )
 
 gg_temporal <- arrangeGrob(g1, g2)
