a1756099a68edcb01c3d68d6e3ff79562c757085
parksw3 <parksw3@mcmaster.ca>
2020-05-23 15:23:01 -0400

    one more analysis

---
+++ analysis/cmp_compare_underreport.R
@@ -0,0 +1,184 @@
+library(igraph)
+library(dplyr)
+library(bbmle)
+library(epigrowthfit)
+source("../sim/ebola_param_seminr.R")
+source("../R/generation.R")
+source("../R/estimate.R")
+source("../R/empirical.R")
+
+rprob <- 0.3
+
+load("../sim/cmp_seir_sim.rda")
+
+intrinsic_fun <- function(tau) {
+	sigma*gamma/(gamma-sigma) * (exp(-sigma*tau)-exp(-gamma*tau))
+}
+
+conditional_fun <- function(tau) {
+	sigma*(gamma+lambda)/(gamma+lambda-sigma) * (exp(-sigma*tau)-exp(-(gamma+lambda)*tau))
+}
+
+true.var <- integrate(function(x) (x-true.mean)^2 * intrinsic_fun(x), 0, Inf)[[1]]
+
+true.shape <- true.mean^2/true.var
+
+cmpGraph <- read.table("../data/CA-CondMat.txt")
+cmpGraph <- graph.data.frame(cmpGraph)
+cmpGraph <- as.undirected(cmpGraph)
+cmpGraph <- delete_vertex_attr(cmpGraph, "name")
+
+d <- degree(cmpGraph)
+
+kappa <- var(d)/mean(d) + mean(d) -1
+
+## This is SEIR
+R0fun.network <- function(r) (gamma+r)/(gamma*sigma/(sigma+r)+r/kappa)
+
+## This is SE2IR
+## R0fun.network <- function(r) {
+##	lambda <- (2 * sigma+r)^2 * (r + gamma)/((kappa-1) * 4 * sigma^2 - 4 * sigma * r - r^2)
+##	
+##	kappa * lambda/(lambda + gamma)
+## }
+
+censor.gi <- lapply(
+	reslist
+	, network.generation
+	, plot=FALSE
+	, interval=c(10, 1010)
+	, interval.type="cases"
+)
+
+count <- 0
+
+growth <- lapply(reslist, function(x){
+	count <<- count + 1
+	print(count)
+	day <- floor(x$data$time) + 1
+	
+	fitdata <- data.frame(
+		day=unique(day),
+		cases=as.vector(table(day))
+	)
+	
+	ee <- epigrowthfit(time=fitdata$day,
+				 deaths=fitdata$cases,
+				 model="logistic")
+	
+	dd <- data.frame(r=coef(ee)[1])
+	
+	dd
+}) %>%
+	bind_rows(.id="sim")
+
+set.seed(101)
+individual.est <- lapply(reslist, function(x){
+	tmax <- x$data[1001,1]
+	gendata <- generation.data(x, tmax=tmax)
+	
+	ww <- rbinom(1:nrow(gendata), 1, rprob)==1
+	
+	gendata$infected_by[ww] <- NA
+	gendata$generation[ww] <- NA
+	
+	est <- as.data.frame(t(parametricfun(5, 10, 2, gendata, tmax)))
+	
+	est$type <- c("RR", "mean", "shape")
+	
+	print(est)
+	
+	est
+}) %>%
+	bind_rows(.id="sim")
+
+individual.RR <- lapply(1:100, function(x){
+	gg <- growth[x,2]
+	
+	mean <- filter(individual.est, type=="mean")[x,2]
+	shape <- filter(individual.est, type=="shape")[x,2]
+	
+	(1 + gg * 1/shape * mean)^(shape)
+})
+
+set.seed(101)
+population.est <- lapply(1:100, function(x){
+	cg <- censor.gi[[x]]
+	gg <- growth[x,2]
+	
+	ww <- rbinom(1:length(cg), 1, rprob)==1
+	
+	populationfun(cg[ww], gg)
+}) %>%
+	bind_rows(.id="sim")
+
+observed.est <- lapply(1:100, function(x){
+	cg <- censor.gi[[x]]
+	gg <- growth[x,2]
+	
+	data.frame(
+		estimate=c(mean(cg), 1/mean(exp(-gg*cg))),
+		type=c("mean", "RR")
+	)
+}) %>%
+	bind_rows(.id="sim")
+
+intrinsic.est <- lapply(growth$r, function(x) 1/mean(exp(-x*gen)))
+
+network.est <- lapply(growth$r, R0fun.network)
+
+empirical.est <- lapply(reslist, empirical.R0, 100)
+
+count <- 0
+
+forward.est <- sapply(reslist, function(x) {
+  count <<- count + 1
+  n <- 75
+  
+  o <- order(x$t_infected)
+  tmax <- tail(x$data$time, 1)
+  
+  n <- min(n, sum(x$t_recovered[o] < tmax, na.rm=TRUE))
+  o <- o[1:n]
+  
+  forwardgen <- unlist(lapply(o, function(v) {
+    x$t_infected[which(x$infected_by==v)]-x$t_infected[v]
+  }))
+  
+  1/mean(exp(-growth$r[count] * forwardgen))
+})
+
+forward.gen <- sapply(reslist, function(x) {
+  n <- 75
+  
+  o <- order(x$t_infected)
+  tmax <- tail(x$data$time, 1)
+  
+  n <- min(n, sum(x$t_recovered[o] < tmax, na.rm=TRUE))
+  o <- o[1:n]
+  
+  forwardgen <- unlist(lapply(o, function(v) {
+    x$t_infected[which(x$infected_by==v)]-x$t_infected[v]
+  }))
+  
+  mean(forwardgen)
+})
+
+RRdata <- data.frame(
+	observed=observed.est$estimate[observed.est$type=="RR"],
+	individual=unlist(individual.RR),
+	population=population.est$estimate[population.est$type=="RR"],
+	intrinsic=unlist(intrinsic.est),
+	network=unlist(network.est),
+	forward=forward.est,
+	empirical=unlist(empirical.est)
+)
+
+gendata <- data.frame(
+	observed=observed.est$estimate[observed.est$type=="mean"],
+	individual=individual.est$estimate[individual.est$type=="mean"],
+	population=population.est$estimate[population.est$type=="mean"],
+	forward=forward.gen
+)
+
+save("RRdata", "gendata", file="cmp_compare_underreport.rda")
---
+++ fig/cmp_underreport.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2); theme_set(theme_bw())
+library(gridExtra)
+
+source("../sim/ebola_param_seminr.R")
+
+scale_colour_discrete <- function(...,palette="Dark2") scale_colour_brewer(...,palette=palette)
+scale_fill_discrete <- function(...,palette="Dark2") scale_fill_brewer(...,palette=palette)
+
+load("../analysis/cmp_compare_underreport.rda")
+
+RRgroup <- data.frame(
+	key=c("contact\ntracing", "population\ncorrection", "individual\ncorrection", "initial\nforward", "empirical", "egocentric", "intrinsic"),
+	group=factor(c("tracing based", "tracing based", "tracing based", "empirical", "empirical", "individual based", "individual based"),
+				 levels=c("tracing based", "empirical", "individual based"))
+)
+
+RRdata2 <- RRdata %>%
+	gather(key, value) %>%
+	mutate(key=factor(key, 
+					  levels=c("observed", "population", "individual", "forward", "empirical", "network", "intrinsic"),
+					  labels=c("contact\ntracing", "population\ncorrection", "individual\ncorrection", 
+					  		 "initial\nforward", "empirical", "egocentric", "intrinsic"))) %>%
+	merge(RRgroup)
+
+ggR <- ggplot(RRdata2) +
+	geom_boxplot(aes(key, value, fill=key), alpha=0.7) +
+	scale_y_log10("Reproductive number", breaks=c(1, 2, 4, 8, 16)) +
+	facet_grid(~group, scale="free", space="free_x") +
+	theme(
+		panel.spacing.x = unit(0, "lines"),
+		strip.background = element_blank(),
+		legend.position = "none",
+		axis.title.x=element_blank()
+	)
+
+ggsave("cmp_reproductive_underreport.pdf", ggR, width=6, height=3)
